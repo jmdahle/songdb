37ba69e7824544be65fe7113d586b62b00d5c319
JMDahle <47642064+jmdahle@users.noreply.github.com>
2020-07-25 13:26:05 -0500

    initial scaffolding

---
+++ app.js
@@ -0,0 +1,90 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'jdahle',
+    password: 'JD@lama2519',
+    database: 'top_songsDB',
+});
+
+connection.connect(err => {
+    if (err) throw err
+    console.log(`Connected to database on ${connection.threadId}`);
+    initialPrompts();
+})
+
+const killIt = () => {
+    console.log('Closing connections and exiting.');
+    connection.end();
+    process.exit();
+}
+
+const initialPrompts = () => {
+    inquirer.prompt([
+        {
+            name: 'action',
+            message: 'Choose an action',
+            type: 'list',
+            choices: [
+                {name: 'Artist Search', value: 'artist'},
+                {name: 'Multi Search', value: 'multi'},
+                {name: 'Range Search', value: 'range'},
+                {name: 'Song Search', value: 'song'},
+                {name: 'Exit', value: 'exit'}
+            ]
+        }
+    ]).then( answer => {
+        switch (answer.action) {
+            case 'artist':
+                artistSearch();
+                break;
+            case 'song':
+                songSearch();
+                break;
+            case 'multi':
+                multiSearch();
+                break;
+            case 'range':
+                rangeSearch();
+                break;
+            default :
+                console.log('Exiting...');
+                killIt();
+                break;
+        }
+    })
+}
+
+// artirst serach
+// --   A query which returns all data for songs sung by a specific artist
+const artistSearch = () => {
+    console.log('Searching artist...');
+    
+    initialPrompts();
+}
+
+// multiSearch
+// --    A query which returns all artists who appear within the top 5000 more than once
+const multiSearch = () => {
+    console.log('Searching for artists that appear more than x times ...');
+    
+    initialPrompts();
+}
+
+// rangeSearch
+// --    A query which returns all data contained within a specific range
+const rangeSearch = () => {
+    console.log('rangeSearch...');
+    
+    initialPrompts();
+}
+
+// --    A query which searches for a specific song in the top 5000 and returns the data for it
+const songSearch = () => {
+    console.log('Searching for song...');
+    
+    initialPrompts();
+}
+
